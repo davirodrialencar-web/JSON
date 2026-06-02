25a219615432814d77eb7da603ee16c022e1a803
davirodrialencar-web <davirodrialencar@gmail.com>
2026-06-02 13:33:36 -0300

    AULA J.SON2
    02/06

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 
-const dados =  fs.readFileSync('exec1.json','utf-8')
+const dados =  fs.readFileSync('dados.json','utf-8')
 const usuarios = JSON.parse(dados)
 
 console.log("usuarios cadastrados")
 console.log(usuarios)
+
+usuarios.push({id:3, nome:"Josefino",idade:15})
+fs.writeFileSync('dados.json', JSON.stringify(usuarios,null,2))
