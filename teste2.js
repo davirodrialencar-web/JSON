a85493d9aed43bc4cbdc3633b69da71a477a6856
davirodrialencar-web <davirodrialencar@gmail.com>
2026-06-23 17:28:18 -0300

    23/06
    2306

--- teste2.js
+++ teste2.js
@@ -2,9 +2,23 @@ const prompt = require('prompt-sync')()
 const fs = require('fs')//pedindo comandos
 
 let cadastro = 0 
-while (cadastro = 0) {
+while (cadastro === 0) {
     console.log("Comece o seu cadastro!")
-    console.log(parseInt(prompt("Digite sua idade :")))
-    if (cadastro === 1 ) {}
+    console.log(parseInt(prompt("Digite 1 para começar o seu cadastro:")))
+    if (cadastro === 1 ) {
+        const prompt = require('prompt-sync')()
+        const fs = require('fs')
+        
+        let dados = fs.readFileSync('cev.json', 'utf-8')
+        let usuarios = JSON.parse(dados)
+        const nome = prompt("Digite o nome: ")
+        const idade = parseInt(prompt("Digite sua idade :"))
+        
+        const novoUsuario = {
+            id: usuarios.length + 1,
+            nome,
+            idade
+        }
+    }
 }
 
